e187f3b0fa0bac5465349a768145215664a1d650
lalit kumar <lalit.kumar@socialinnovation.ca>
2023-10-15 01:28:46 -0400

    update

---
+++ src/ColorList.jsx
@@ -0,0 +1,11 @@
+import SingleColor from './SingleColor'
+import { nanoid } from 'nanoid'
+export const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />
+      })}
+    </section>
+  )
+}
